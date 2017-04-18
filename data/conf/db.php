a5b236f0501aad85c5d229c84bd6f79f1c8c6919
zhubobin <root@localhost.localdomain>
2017-04-17 21:28:02 -0700

    123

--- data/conf/db.php
+++ data/conf/db.php
@@ -6,11 +6,10 @@ return array(
     'DB_TYPE' => 'mysqli',
     'DB_HOST' => '192.168.1.109',
     'DB_NAME' => 'phonelive',
-<<<<<<< HEAD
-=======
+
 //    'DB_USER' => 'phonelive',
 //    'DB_PWD' => 'tieweishivps',
->>>>>>> 44957bbe60877878268fbcc85720e0bd31ebe8bc
+
     'DB_USER' => 'root',
     'DB_PWD' => 'qq123',
     'DB_PORT' => '3306',
